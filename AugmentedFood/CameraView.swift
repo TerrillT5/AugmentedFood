46ce2e907b0829620f0a2449d9b02c032d13a1ce
TerrillT5 <t.thorne96@yahoo.com>
2017-09-26 09:18:41 -0400

    Image classifier text added

--- AugmentedFood/CameraView.swift
+++ AugmentedFood/CameraView.swift
@@ -18,7 +18,15 @@ import ARKit
 class CameraView: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate, ARSKViewDelegate, UINavigationControllerDelegate {
     
     var previewLayer: AVCaptureVideoPreviewLayer!
-    let barCodeDetector = VNDetectBarcodesRequest()
+    
+    let classifierText: UILabel = {
+        let classifer = UILabel()
+        classifer.translatesAutoresizingMaskIntoConstraints = false
+        classifer.textColor = .black
+        classifer.font = UIFont(name: "Times-New-Roman", size: 10)
+        classifer.textAlignment = .center
+        return classifer
+    }()
     
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
         guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
@@ -27,7 +35,9 @@ class CameraView: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate,
         let request = VNCoreMLRequest(model: model) { (finishedReq, err) in
             guard let results = finishedReq.results as?  [VNClassificationObservation] else { return }
             guard let firstObservation = results.first else { return }
-            print(firstObservation.identifier,firstObservation.confidence)
+            DispatchQueue.main.async {
+              self.classifierText.text = "This appears to be a \(firstObservation.identifier as String)"
+             }
         }
         try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
     }
@@ -35,40 +45,13 @@ class CameraView: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate,
         super.init(nibName: nil, bundle: nil)
         view.backgroundColor = .white
     }
-    
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //        previewView = UIView(frame: CGRect(x: 0,
-        //                                           y: 0,
-        //                                           width: UIScreen.main.bounds.size.width,
-        //                                           height: UIScreen.main.bounds.size.height))
-        //        previewView.contentMode = UIViewContentMode.scaleAspectFit
-        //        view.addSubview(previewView)
-        //
-        //        boxView = UIView(frame: self.view.frame)
-        //        view.addSubview(boxView)
-        //        self.setupAVCapture()
         
-        let classifierText: UILabel = {
-            let classifier = UILabel()
-            classifier.translatesAutoresizingMaskIntoConstraints = false
-            classifier.text = "This appears to be a \(barCodeDetector)"
-            classifier.textColor = .black
-            classifier.font = UIFont(name: "Times-New-Roman", size: 10)
-            classifier.textAlignment = .center
-            return classifier
-        }()
-        //        let imageView: UIImageView = {
-        //            let image = UIImageView()
-        //            image.translatesAutoresizingMaskIntoConstraints = false
-        //            UIGraphicsBeginImageContext(CGSize(width:  299, height: 299))
-        //            return image
-        //        }()
         let imageSession = AVCaptureSession()
         imageSession.sessionPreset = .photo
         guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
@@ -79,19 +62,16 @@ class CameraView: UIViewController,AVCaptureVideoDataOutputSampleBufferDelegate,
         var previewLayer: AVCaptureVideoPreviewLayer?
         previewLayer = AVCaptureVideoPreviewLayer(session: imageSession)
         previewLayer?.frame = view.bounds
-        //        previewLayer.frame =  CGRect(x: 0, y: 64, width: UIScreen.main.bounds.width, height: 350) \\\\ *************
         view.layer.addSublayer(previewLayer!)
         let dataOutput = AVCaptureVideoDataOutput()
         dataOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
         imageSession.addOutput(dataOutput)
-        //
+        
         let classifiedTextTopAnchor = classifierText.topAnchor.constraint(equalTo: view.topAnchor, constant: 500)
         let classifiedTextRightAnchor = classifierText.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20)
         let classifiedTextLeftAnchor = classifierText.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20)
         let classifiedTextHeightAnchor = classifierText.heightAnchor.constraint(equalToConstant: 35)
         
-        
-        //        view.addSubview(imageView)
         view.addSubview(classifierText)
         NSLayoutConstraint.activate([classifiedTextTopAnchor,classifiedTextRightAnchor,classifiedTextLeftAnchor,
                                      classifiedTextHeightAnchor])
--- AugmentedFood/LandingVC.swift
+++ AugmentedFood/LandingVC.swift
@@ -23,21 +23,11 @@ class LandingViewController: UIViewController {
         nameOfApp.translatesAutoresizingMaskIntoConstraints = false
         return nameOfApp
     }()
-    //    let appDescription: UILabel = {
-    //        let description = UILabel()
-    //        description.translatesAutoresizingMaskIntoConstraints = false
-    //        description.textColor = .white
-    //        description.font = UIFont(name: "Times-New Roman" , size: 15)
-    //        description.textAlignment = .center
-    //        description.sizeToFit()
-    //        description.text = "Focus "
-    //        return description
-    //    }()
-    let nextScreenButton: UIButton = {
+        let nextScreenButton: UIButton = {
         let continueButton = UIButton()
         continueButton.layer.cornerRadius = 10
         continueButton.layer.borderWidth = 1
-        //        continueButton.layer.borderColor = UIColor.lightGray.cgColor
+        //         = UIColor.lightGray.cgColor
         continueButton.layer.shadowColor = UIColor.black.cgColor
         continueButton.layer.shadowOffset = CGSize(width: 0.5, height: 15)
         continueButton.layer.shadowRadius = 5
@@ -62,49 +52,21 @@ class LandingViewController: UIViewController {
     @objc func advanceToNextVC(sender: UIButton) {
         let secondViewController = CameraView()
         secondViewController.modalPresentationStyle = .fullScreen
-        // present the view controller
         navigationController?.pushViewController(CameraView(), animated: true)
         
     }
     
-    //        present(secondViewController, animated: true, completion: nil)
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .gray // Change to custom color
         view.addSubview(appName)
-        //        view.addSubview(appDescription)
         view.addSubview(nextScreenButton)
         
-        //        var frameRect = CGRect(x:10, y:30, width: 80, height: 80)
-        //        frameRect = CGRect(x:150, y:20, width:60, height: 60)
-        //        appName = UILabel(frame: frameRect)
-        
-        /// behavior added to AppName
-        //        let animateLabel = UIDynamicAnimator(referenceView: self.view)
-        //        let originalPosition = CGPoint(x: appName.center.x, y: appName.center.y)
-        //        appName.center = CGPoint(x: view.frame.width / 2, y: -view.frame.height)
-        //        snap = UISnapBehavior(item: appName, snapTo: originalPosition)
-        //        snap.damping = 0.5
-        //        animateLabel.addBehavior(snap)
-        //        self.view.layoutIfNeeded()
-        
-        //        animateLabel = UIDynamicAnimator(reference: self.view)
-        
-        //        let gravity = UIGravityBehavior(items: [appName])
-        //        let vector  = CGVector(dx: 0.2, dy: 1.0)
-        //        gravity.gravityDirection = vector
-        //        animateLabel.addBehavior(gravity)
-        
-        let topAppNameAnchor = appName.topAnchor.constraint(equalTo: view.topAnchor, constant: 75)
+              let topAppNameAnchor = appName.topAnchor.constraint(equalTo: view.topAnchor, constant: 75)
         let rightAppNameAnchor = appName.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -50)
         let leftAppNameAnchor = appName.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 50)
         let heightAppNameAnchor = appName.heightAnchor.constraint(equalToConstant: 45)
-        //
-        //            let topAppDescriptionAnchor = appDescription.topAnchor.constraint(equalTo: appName.bottomAnchor , constant: 100)
-        //            let rightAppDescriptionAnchor = appDescription.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 20)
-        //            let leftAppDescriptionAnchor = appDescription.leftAnchor.constraint(equalTo: view.leftAnchor, constant: -20)
-        //            let heightAppDescription = appDescription.heightAnchor.constraint(equalToConstant: 45)
         
         let topButtonAnchor = nextScreenButton.topAnchor.constraint(equalTo: appName.bottomAnchor, constant: 45)
         let rightButtonAnchor = nextScreenButton.rightAnchor.constraint(equalTo: view.rightAnchor , constant: 20)
