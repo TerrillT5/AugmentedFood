246d4ccc4d9e82439b28a0d42854ef19daff412f
TerrillT5 <t.thorne96@yahoo.com>
2017-10-12 14:01:38 -0400

    Camera flash added
    separate camera button added ,

---
+++ AugmentedFood/CameraViewError.swift
@@ -0,0 +1,20 @@
+//
+//  CameraError.swift
+//  AugmentedFood
+//
+//  Created by Terrill Thorne on 10/12/17.
+//  Copyright © 2017 Terrill Thorne. All rights reserved.
+//
+import Foundation
+ struct CameraViewError  {
+    enum Constants {
+        case captureSessionAlreadyRunning
+        case captureSessionIsMissing
+        case inputsAreInvalid
+        case invalidOperation
+        case noCameraAvailable
+        case unknown
+    }
+}
+
+
